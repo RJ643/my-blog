4b324670502d19c3e032b819e964f39df633ec2c
rj643 <hello.git.deucalion1002@gmail.com>
2025-03-22 20:57:10 +0900

    시용자 아바타

--- blog/models.py
+++ blog/models.py
@@ -67,6 +67,12 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://ui-avatars.com/api/?name={self.author.username}&size=45'
+
     def __str__(self):
         return f'{self.author}::{self.content}'
 
