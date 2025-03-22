1533f57ffff73056591023139f414812db37b599
rj643 <hello.git.deucalion1002@gmail.com>
2025-03-22 18:43:59 +0900

    댓글 삭제기능

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,13 @@ from . import views
 from .views import instant_logout
 
 urlpatterns = [
+    path('delete_comment/<int:pk>/', views.delete_comment, name='delete_comment'),
+    path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<path:slug>/', views.tag_page, name='tag_page'),
     path('category/<path:slug>/', views.category_page, name='category_page'),  # ✅ 변경: <slug:slug> → <path:slug>
+    path('<int:pk>/new_comment/', views.new_comment),
     path('<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
     path('', views.PostList.as_view(), name='post_list'),
     path('instant-logout/', instant_logout, name='instant_logout'),
--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,8 @@ from traitlets.config.manager import recursive_update
 from django.utils.text import slugify
 from django.contrib.auth import logout
 
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
+from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
 
 # 📌 [1] 메인 페이지 (게시글 리스트)
@@ -29,6 +30,14 @@ class PostDetail(DetailView):
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
+
+        comment_form = CommentForm()
+        comment_form.fields['content'].widget.attrs.update({
+            'placeholder': 'Join the discussion and leave a comment!',
+            'class': 'form-control',
+            'rows': '3'
+        })
+        context['comment_form'] = comment_form
         return context
 
 
@@ -154,3 +163,38 @@ def instant_logout(request):
         """GET 요청만으로 즉시 로그아웃하는 뷰"""
         logout(request)
         return redirect('/blog')  # 로그아웃 후 홈으로 이동 (필요에 따라 변경 가능)
+def new_comment(request,pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
+
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+        else:
+            return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
